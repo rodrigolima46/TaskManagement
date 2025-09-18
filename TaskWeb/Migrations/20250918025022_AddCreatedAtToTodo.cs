0d4906b94f91f94dbe547cf546fe136f034ae53f
Rodrigo Lima Cavalcante <rodrigo_lima_c@hotmail.com>
2025-09-17 23:28:11 -0400

    adding filters

---
+++ TaskWeb/Migrations/20250918025022_AddCreatedAtToTodo.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedAtToTodo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "TodoItems",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "TodoItems",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
